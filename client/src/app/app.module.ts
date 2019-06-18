d4062ffe4b78648477563a070477a68c21cd4d0a
saikumarpuli <saikumarpuli9@gmail.com>
2019-06-18 19:57:09 +0530

    project setup completed

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SeatsComponent } from './seats/seats.component';
+import {AngularFontAwesomeModule} from "angular-font-awesome";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeatsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
